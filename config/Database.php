a9442178a164a60866642adeb64d398aca24767f
TXBIT <shenlong203@gmail.com>
2020-07-24 18:45:46 +0700

    config - Database, models - Post, api - post - read

---
+++ config/Database.php
@@ -0,0 +1,26 @@
+<?php
+  class Database {
+    // DB Params
+    // In docker, the database host is not localhost but the container name of the database service
+    private $host = 'php-rest-blog_database_1';
+    private $port = '3306';
+    private $db_name = 'my-blog';
+    private $username = 'root';
+    private $password = '123456';
+    private $conn;
+
+    // DB Connect
+    public function connect() {
+      $this->conn = null;
+
+      try {
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';port=' . $this->port, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+
+      return $this->conn;
+    }
+  }
+?>
